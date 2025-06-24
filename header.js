041136e4c23eeb6aced7095ba6c25ef36ae1ae7e
RAJESHKANTHASAMY <smewsince2006@gmail.com>
2025-06-24 21:08:04 +0530

    fourth

---
+++ header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="header">
+      <img 
+      style={
+        {
+          height: '100px',
+          width : '200px'
+        }
+      }
+      src="/nstagram.png" alt="Instagram Logo" />
+    </div>
+  );
+}
+
+export default Header;
---
+++ script.js
@@ -0,0 +1,3 @@
+function sgfg() {
+            alert("oombu");
+        }
